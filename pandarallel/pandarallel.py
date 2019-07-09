5e68a157f5ad774fd75eb6e33729ce4622fb6f5a
Manu NALEPA <nalepae@gmail.com>
2019-07-09 18:48:04 +0200

    Implement quiet mode & Delete previous plasma_store_server process before creating a new one

--- pandarallel/pandarallel.py
+++ pandarallel/pandarallel.py
@@ -9,15 +9,18 @@ from .series import Series
 from .series_rolling import SeriesRolling
 from .rolling_groupby import RollingGroupby
 from .dataframe_groupby import DataFrameGroupBy
+from .plasma_store import start_plasma_store
 
-SHM_SIZE_MB = int(2e3) # 2 GB
+SHM_SIZE_MB = int(2e3)  # 2 GB
 NB_WORKERS = multiprocessing.cpu_count()
 PROGRESS_BAR = False
+
 
 class pandarallel:
     @classmethod
     def initialize(cls, shm_size_mb=SHM_SIZE_MB, nb_workers=NB_WORKERS,
-                   progress_bar=False, progress_bar_script=False):
+                   progress_bar=False, progress_bar_script=False,
+                   verbose=2):
         """
         Initialize Pandarallel shared memory.
 
@@ -34,10 +37,6 @@ class pandarallel:
             WARNING: Progress bar is an experimental feature.
                      This can lead to a considerable performance loss.
         """
-
-        print("New pandarallel memory created - Size:", shm_size_mb, "MB")
-        print("Pandarallel will run on", nb_workers, "workers")
-
         if progress_bar or progress_bar_script:
             print("WARNING: Progress bar is an experimental feature. This \
 can lead to a considerable performance loss.")
@@ -47,21 +46,34 @@ can lead to a considerable performance loss.")
             else:
                 tqdm_notebook().pandas()
 
-        cls.__store_ctx = plasma.start_plasma_store(int(shm_size_mb * 1e6))
-        plasma_store_name, _ = cls.__store_ctx.__enter__()
+        verbose_store = verbose >= 2
+
+        if hasattr(cls, "proc"):
+            cls.proc.kill()
+
+        if verbose >= 1:
+            print("New pandarallel memory created - Size:", shm_size_mb, "MB")
+            print("Pandarallel will run on", nb_workers, "workers")
+
+        plasma_store_name, cls.proc = start_plasma_store(int(shm_size_mb * 1e6),
+                                                         verbose=verbose_store)
 
         plasma_client = plasma.connect(plasma_store_name)
 
         args = plasma_store_name, nb_workers, plasma_client
 
         pd.DataFrame.parallel_apply = DataFrame.apply(*args, progress_bar)
-        pd.DataFrame.parallel_applymap = DataFrame.applymap(*args, progress_bar)
+        pd.DataFrame.parallel_applymap = DataFrame.applymap(
+            *args, progress_bar)
 
         pd.Series.parallel_map = Series.map(*args, progress_bar)
         pd.Series.parallel_apply = Series.apply(*args, progress_bar)
 
-        pd.core.window.Rolling.parallel_apply = SeriesRolling.apply(*args, progress_bar)
+        pd.core.window.Rolling.parallel_apply = SeriesRolling.apply(
+            *args, progress_bar)
 
-        pd.core.groupby.DataFrameGroupBy.parallel_apply = DataFrameGroupBy.apply(*args)
+        pd.core.groupby.DataFrameGroupBy.parallel_apply = DataFrameGroupBy.apply(
+            *args)
 
-        pd.core.window.RollingGroupby.parallel_apply = RollingGroupby.apply(*args)
+        pd.core.window.RollingGroupby.parallel_apply = RollingGroupby.apply(
+            *args)
